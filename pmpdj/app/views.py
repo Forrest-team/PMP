c9d822807f804379a337502a4630da2a4b1f3a20
Violet-maple <llwao_mail@163.com>
2018-08-01 15:58:13 +0800

    'origin.'

--- pmpdj/app/views.py
+++ pmpdj/app/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+
+    return render(request, 'index.html')
+
+
+def login(request):
+    """
+    业主登录
+    :param request:
+    :return:
+    """
+    return render(request, 'login.html')
